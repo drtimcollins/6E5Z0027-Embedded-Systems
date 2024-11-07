85add05a5a8798be554f4bb2ff7a645be2b81d67
Tim Collins <47898052+drtimcollins@users.noreply.github.com>
2024-11-07 22:09:49 +0000

    Update Ex06-Keypad.py

--- Workshop7/Ex06-Keypad.py
+++ Workshop7/Ex06-Keypad.py
@@ -1,28 +1,24 @@
 from machine import Pin, Timer
 
+# Rows are connected as inputs with an internal pull-up resistor.
 rowPins = (Pin(26, Pin.IN, Pin.PULL_UP), Pin(22, Pin.IN, Pin.PULL_UP), 
             Pin(21, Pin.IN, Pin.PULL_UP), Pin(20, Pin.IN, Pin.PULL_UP))
+# Columns are Open Drain outputs: grounded when low, open circuit when high.
 colPins = (Pin(19, Pin.OPEN_DRAIN), Pin(18, Pin.OPEN_DRAIN),
             Pin(17, Pin.OPEN_DRAIN), Pin(16, Pin.OPEN_DRAIN))
+# Lookup table for the meaning of each key.
 keyValues = ("123A","456B","789C","*0#D")
+# Storage for the state of each key. Used to detect changes in button presses.
+keyStates = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
 
-def scanKeys():    
-    for col in range(4):
-        colPins[col].value(0)
-        for row in range(4):
-            if rowPins[row].value() == 0:
-                colPins[col].value(1)
-                return keyValues[row][col]
-        colPins[col].value(1)
-    return ""
+def scanKeys(timer):  
+    for col in range(4):                                      # Scan through all 4 columns
+        colPins[col].value(0)                                 # Connects the column to GND
+        for row in range(4):                                  # Scan through all 4 rows
+            if rowPins[row].value() != keyStates[row][col]:   # Only looking for changes
+                keyStates[row][col] = rowPins[row].value()    # Store new key state
+                if keyStates[row][col] == 0:                  # Respond if new state is 0,
+                   print(keyValues[row][col], end="")         # i.e. connected to GND.
+        colPins[col].value(1)                                 # Set column to 'floating'
 
-lastKey = ""
-def updateKeys(timer):
-    global lastKey
-    newKey = scanKeys()
-    if lastKey != newKey:
-        lastKey = newKey
-        if len(lastKey) > 0:
-            print(lastKey,end="")
-
-tmr = Timer(freq = 100, callback=updateKeys)
+tmr = Timer(freq = 100, callback=scanKeys)
