672adbbee72e8bafc915499cbf6c9d0d7e5ddc3e
Tim Collins <47898052+drtimcollins@users.noreply.github.com>
2025-09-05 13:18:23 +0100

    Update SmartBattleShips3.py

--- Workshop4/SmartBattleShips3.py
+++ Workshop4/SmartBattleShips3.py
@@ -1,17 +1,11 @@
+import numpy as np
 import BattleShips
 
 # Chooses cells in order.
 class SmartBattleShips(BattleShips.BattleShips):
-    def newGame(self):
-        self.nextGuess = (0,0)                              # Reset next guess at the start of a game
-
     def guess(self):
-        thisGuess = self.nextGuess                          # Copy current nextGuess value
-        if thisGuess[1] < self.N - 1:                       # Check for the end of the row...
-            self.nextGuess = (thisGuess[0],thisGuess[1]+1)  # ...if not there, then move one cell onwards.
-        else:
-            self.nextGuess = (thisGuess[0] + 1, 0)          # Row done so start next row and reset column to 0
-        return thisGuess
+        options = np.where(self.map == ' ')                # Finds coordinates of all untested cells
+        return (options[0][0],options[1][0])               # Returns coordinates of the first match
 
 b = SmartBattleShips()
 b.gui()
