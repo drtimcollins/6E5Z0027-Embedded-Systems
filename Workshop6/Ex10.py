b0dad8542bc65d498b65f12750cb0ae3642d89af
Tim Collins <47898052+drtimcollins@users.noreply.github.com>
2026-04-14 13:03:01 +0100

    Rename sendByte to sendBytes and update callback

--- Workshop6/Ex10.py
+++ Workshop6/Ex10.py
@@ -4,7 +4,7 @@ uart = UART(0, baudrate=1000, tx=Pin('GP0'), rx=Pin('GP1'))
 btn1 = Pin('GP17', Pin.IN)             # Set up input pins for buttons 1 & 2
 btn2 = Pin('GP16', Pin.IN)
 
-def sendByte(timer):
+def sendBytes(timer):
    if btn1.value() and btn2.value():
       uart.write('?f')        # Transmit '?':0b00111111, 'f':0b01100110
    elif btn1.value():
@@ -15,4 +15,4 @@ def sendByte(timer):
       uart.write('??')        # Transmit '?':0b00111111, '?':0b00111111
 
 # Set up a timer to trigger every 50 ms
-tmr = Timer(period = 50, callback = sendByte)
+tmr = Timer(period = 50, callback = sendBytes)
