bdd714a8bbd1f553e3fcdc4339e355a9f7201170
Tim Collins <47898052+drtimcollins@users.noreply.github.com>
2026-08-14 10:54:05 +0100

    Comments added

--- Workshop4/SmartTilist02.py
+++ Workshop4/SmartTilist02.py
@@ -4,32 +4,26 @@ from Tilist import Tilist
 
 class TilistPlayer(Tilist):
     def autoPlay(self):
-        spaces = np.argwhere(self.grid == '  ')
+        spaces = np.argwhere(self.grid == '  ')    # Make list of available spaces
         
-        if self.roundNumber < 3:
+        if self.roundNumber < 3:                   # For rounds 0-2, choose at random
             indices = np.random.choice(np.shape(spaces)[0], 4, replace=False)
-        else:      
+        else:
+            # Initialise the best score found so far to nothing
             bestScore = 0
             indices = np.arange(4)
 
+            # Iterate through every permutation of possible placements 
             for trialIndices in permutations(range(np.shape(spaces)[0]), 4):
-                testGrid = self.grid.copy()
-                
-                for i,tile in zip(trialIndices, self.hand):
-                    testGrid[tuple(spaces[i])] = tile
+                testGrid = self.grid.copy()                  # Copy current grid so we can
+                                                             # try out this permutation.
+                for i,tile in zip(trialIndices, self.hand):  # Place each tile in the hand
+                    testGrid[tuple(spaces[i])] = tile        # into the testGrid.
 
-                score = self.calculateScores(testGrid)
-                
-                print(trialIndices, score)
-                print(testGrid)
-                
-                if score > bestScore:
-                    bestScore = score
-                    indices = np.array(trialIndices)
+                score = self.calculateScores(testGrid)       # Score this test placement
+                                
+                if score > bestScore:                        # If it beats the best so far
+                    bestScore = score                        # then set this score as the
+                    indices = np.array(trialIndices)         # new best and store indices.
 
-        print(indices)
-        print(type(indices))
-        return spaces[indices,:]
-
-mp = TilistPlayer()
-mp.gui()
+        return spaces[indices]                               # Return coordinates as before
