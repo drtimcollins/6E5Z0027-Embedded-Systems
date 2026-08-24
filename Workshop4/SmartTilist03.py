f7e460990ea3ed487c73b52288b9a0fd7bee1b06
Tim Collins <47898052+drtimcollins@users.noreply.github.com>
2026-08-24 09:27:35 +0100

    Update SmartTilist03.py

--- Workshop4/SmartTilist03.py
+++ Workshop4/SmartTilist03.py
@@ -3,16 +3,30 @@ import numpy as np
 from Tilist import Tilist
 
 class TilistPlayer(Tilist):
+    def __init__(self, Ntries):
+        self.Nt = Ntries
+        self.initGameData()
     def autoPlay(self):
         spaces = np.argwhere(self.grid == '  ')    # Make list of available spaces
-        
-        if self.roundNumber < 3:                   # For rounds 0-2, choose at random
-            indices = np.random.choice(np.shape(spaces)[0], 4, replace=False)
-        else:
-            # Initialise the best score found so far to nothing
-            bestScore = 0
-            indices = np.arange(4)
+ 
+         # Initialise the best score found so far to nothing
+        bestScore = 0
+        indices = np.arange(4)
 
+        if self.roundNumber < 3:                   # For rounds 0-2, choose at random
+            for trial in range(self.Nt):
+                trialIndices = np.random.choice(np.shape(spaces)[0], 4, replace=False)
+                testGrid = self.grid.copy()                  # Copy current grid so we can
+                                                             # try out this permutation.
+                for i,tile in zip(trialIndices, self.hand):  # Place each tile in the hand
+                    testGrid[tuple(spaces[i])] = tile        # into the testGrid.
+
+                score = self.calculateScores(testGrid)       # Score this test placement
+                                
+                if score > bestScore:                        # If it beats the best so far
+                    bestScore = score                        # then set this score as the
+                    indices = np.array(trialIndices)         # new best and store indices.
+        else:
             # Iterate through every permutation of possible placements 
             for trialIndices in permutations(range(np.shape(spaces)[0])):
                 testGrid = self.grid.copy()                  # Copy current grid so we can
@@ -27,3 +41,8 @@ class TilistPlayer(Tilist):
                     indices = np.array(trialIndices)         # new best and store indices.
 
         return spaces[indices]                               # Return coordinates as before
+
+for n in range(2,22,2):
+    t = TilistPlayer(n)
+    print(f'{n}, {t.playGames(500)}')
+      
